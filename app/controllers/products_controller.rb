cc8a8d246a2c72a51f3adf0ec885be8ad4e2d370
Roberto Barros <roberto.f.barros@gmail.com>
2021-12-21 22:51:17 -0300

    authenticate user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   before_action :set_product, only: %i[show]
   def index
     @products = Product.in_auction.all
